9a7f33e4c8fcdb382f1f24cef8e9917003894b7b
srirangan9224 <109905086+srirangan9224@users.noreply.github.com>
2026-04-06 19:40:44 -0400

    changes ADCPeripheral to control LTC2308_ctrl.vhd
    This update includes a detailed description of the ADCPeripheral functionality and integrates the LTC2308 controller component. It also defines state machine modes and outlines process blocks for reading and writing instructions.

--- ADCTest/ADCPeripheral.vhd
+++ ADCTest/ADCPeripheral.vhd
@@ -1,13 +1,22 @@
--- TODO: HEADER COMMENTS HERE 
+-- ADCPeripheral.vhd
+--
+-- This file defines a peripheral that utilizes the LTC2308 file
+-- to get data from the ADC and samples the ADC frequently to get
+-- analog values. It returns a latched value to SCOMP when a read 
+-- instruction is called, and changes the latched value each time 
+-- the ADC is sampled. It also does something similar to rewrite 
+-- the SDI signal based on user input during a write instruction.
+--
 
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
+
 
 entity ADCPeripheral IS 
 	PORT(
 		io_addr 		: IN STD_LOGIC_VECTOR(10 DOWNTO 0); -- address
 		io_write		: IN STD_LOGIC; -- write enable to set SDI config
-		io_data 		: IN STD_LOGIC_VECTOR(2 DOWNTO 0); -- io_data config
+		io_data 		: IN STD_LOGIC_VECTOR(11 DOWNTO 0); -- io_data config
 		resetn  		: IN STD_LOGIC; -- reset signal
 		clk	  		: IN STD_LOGIC; -- clock signal
 		digitalRes	: OUT STD_LOGIC_VECTOR(11 DOWNTO 0) -- resolution of the peripheral
@@ -16,46 +25,69 @@ END ADCPeripheral;
 
 
 ARCHITECTURE arch OF ADCPeripheral IS
-	-- define state machine
-	TYPE mode_type IS (); -- TODO: define states
+-- define the LTC_2308_ctrl file as a component of the peripheral
+component LTC2308_ctrl
+	generic(
+	CTRL_DIV : integer := 1
+	);
+	port (
+		-- define the same ports from LTC_2308_ctrl.vhd
+		-- Control and data for this device
+		clk      : in  std_logic;
+		nrst     : in  std_logic;
+		start    : in  std_logic;
+		rx_data  : out std_logic_vector(11 downto 0);
+		busy     : out std_logic;
+		
+		-- SPI Physical Interface
+		sclk     : out std_logic; -- Serial clock
+		conv     : out std_logic; -- Conversion start control
+		mosi     : out std_logic; -- Data out from this device, in to ADC
+		miso     : in  std_logic  -- Data out from ADC, in to this device
+	);
 	
-	-- signal to track modes and output
-	SIGNAL mode		  : mode_type; -- tracks current state
-	SIGNAL digitalRes: STD_LOGIC_VECTOR(11 DOWNTO 0);
-
-	-- signals from register map of ADC
-	SIGNAL odd			: STD_LOGIC := '0'; -- can modify
-	SIGNAL single		: STD_LOGIC := '1';
-	SIGNAL selectOne	: STD_LOGIC := '0'; -- can modify
-	SIGNAL SelectZero	: STD_LOGIC := '0'; -- can modify
-	SIGNAL unipolar	: STD_LOGIC := '1';
-	SIGNAL sleep		: STD_LOGIC := '0';
+	-- creating an instance of the controller with a port map
+	ADC : LTC2308_ctrl
+	port map(
+		clk  => clk,
+		nrst => nrst,
+		start => start,
+		rx_data=> rx_data,
+		busy => busy,
+		sclk => sclk,
+		conv => conv,
+		mosi => mosi,
+		miso => miso
+	);
 	
-	-- describe a signal for SDI
-	SIGNAL SDI			: STD_LOGIC_VECTOR(5 DOWNTO 0);
+	-- create more signals based on what we need the peripheral to do
+	signal latched_SDI	: 	STD_LOGIC_VECTOR(11 DOWNTO 0)
+	signal latched_SDO	:  STD_LOGIC_VECTOR(11 DOWNTO 0)
 	
-	-- buffer to store temporary value
-	SIGNAL latched_SDO: STD_LOGIC_VECTOR(11 DOWNTO 0);
-	SIGNAL latched_SDI: STD_LOGIC_VECTOR(5 DOWNTO 0);
+	-- define a state machine to interact with controller and SCOMP
+	TYPE MODE_TYPE IS (IDLE, RUNNING);
+	signal mode				: MODE_TYPE; -- define a signal to control state
 	
-	-- internal signal to update latched_SDO
-	SIGNAL updateSDO	: STD_LOGIC:= '0';
-	SIGNAL updateSDI	: STD_LOGIC:= '0';
-	SIGNAL toggleWrite: STD_logic:= '0';
-	
-	
+	-- create a process block to define the peripheral
+	PROCESS(clk)
 	BEGIN
-	-- control signal for latching SDO value
-	updateSDO <= '1' -- TODO: complete this definition
+		-- write lines to make the controller sample data 
+		-- from ADC by toggling start and checking busy flag
+		-- check busy flag and update the data in MOSI
+		-- check busy flag and get data to update Latched SDO
+		
+	END
 	
-	-- assign SDI based on input (io_data)
-	odd <= io_data(2);
-	selectZero <= io_data(1);
-	selectOne <= io_data(0);
-	
-	-- TODO: write a process block to define how the peripheral works
-	
-	
-	-- define SDI 
-	SDI <= single & odd & selectOne & selectZero & unipolar & sleep;
+	-- create a process block to define the read instruction of SCOMP
+	PROCESS(clk, nrst, busy)
+		--  nrst is on but busy is on dont reset, else reset
+		-- if IO_READ is true return data from latched,SDO at datares
+	BEGIN
+	END
+	-- create a process to define the write instruction of the SCOMP
+	PROCESS(clk, nrst, busy)
+	BEGIN
+		-- if nrst is on but busy is on dont reset, else reset
+		-- if IO_WRITE is true rewrite data in latched_SDO
+	END
 END arch;
